62603709cf9b87354a1b9dcab3ab0634dba0d9dd
sanaminaaj <86563339+sanaminaaj@users.noreply.github.com>
2021-07-02 20:34:20 +0530

    Create MainActivity3.kt

---
+++ MainActivity3.kt
@@ -0,0 +1,24 @@
+package com.example.studentregistration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+
+        val login= findViewById(R.id.login) as Button
+        login.setOnClickListener{
+            val name=findViewById(R.id.text1) as EditText
+            val password=findViewById(R.id.text2) as EditText
+            if(name.text.toString().equals("admin")&&password.text.toString().equals("admin"))
+                Toast.makeText(applicationContext, "This is a valid ", Toast.LENGTH_LONG).show()
+            else
+                Toast.makeText(applicationContext, "This is not valid", Toast.LENGTH_LONG).show()
+        }
+    }
+}
