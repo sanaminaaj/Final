8e04470b07f5d99b7c7c82371fafb6e95fed0576
sanaminaaj <86563339+sanaminaaj@users.noreply.github.com>
2021-07-12 19:08:59 +0530

    Update MainActivity5.kt

--- MainActivity5.kt
+++ MainActivity5.kt
@@ -5,19 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
-import androidx.core.app.ShareCompat
+import android.widget.Toast
 
 class MainActivity5 : AppCompatActivity() {
-    private lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main5)
-
-        button=findViewById(R.id.button3)
-        button.setOnClickListener()
-        {
-            val intent = Intent(this, MainActivity6::class.java)
-            startActivity(intent)
+        setContentView(R.layout.activity_main9)
+        val login= findViewById(R.id.login) as Button
+        login.setOnClickListener{
+            val name=findViewById(R.id.text1) as EditText
+            val password=findViewById(R.id.text2) as EditText
+            if(name.text.toString().equals("student")&&password.text.toString().equals("student"))
+            {
+                Toast.makeText(applicationContext, "This is a valid ", Toast.LENGTH_LONG).show()
+                val intent= Intent(this,MainActivity8::class.java)
+                startActivity(intent)
+            }
+            else
+                Toast.makeText(applicationContext, "This is not valid", Toast.LENGTH_LONG).show()
         }
-}}
+    }
+}
