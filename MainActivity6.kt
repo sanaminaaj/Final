c011f95d781d45d6314a7139c19073aa39e5ca58
sanaminaaj <86563339+sanaminaaj@users.noreply.github.com>
2021-07-25 20:04:10 +0530

    Create MainActivity6.kt

---
+++ MainActivity6.kt
@@ -0,0 +1,14 @@
+package com.example.projectwork
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity6 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main6)
+
+    }
+}
