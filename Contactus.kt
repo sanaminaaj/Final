3895764202e769f0d28b87eecd07438fe67f5ae0
sanaminaaj <86563339+sanaminaaj@users.noreply.github.com>
2021-07-25 19:45:01 +0530

    Update Contactus.kt

--- Contactus.kt
+++ Contactus.kt
@@ -1,31 +1,27 @@
-package com.example.studentregistration
+package com.example.projectwork
 
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.app.ShareCompat
-class Contactus : AppCompatActivity() {
-    private lateinit var button: Button
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+class Contactus1 : AppCompatActivity() {
+               private lateinit var button: Button
+               override fun onCreate(savedInstanceState: Bundle?) {
+                   super.onCreate(savedInstanceState)
+                   setContentView(R.layout.activity_contactus1)
+                   val button:Button=findViewById(R.id.buttonSend)
+                   val subject:EditText=findViewById(R.id.editTextSubject)
+                   val message:EditText=findViewById(R.id.editTextMessage)
+                   button.setOnClickListener()
+                   {
+                       Toast.makeText(applicationContext, "Choose only Gmail option ", Toast.LENGTH_LONG).show()
+                       ShareCompat.IntentBuilder.from(this).setText(message.text.toString()).setSubject(subject.text.toString()).setEmailTo(arrayOf("sanafarooqui2002@gmail.com")).setChooserTitle("share to").setType("plain/text").startChooser()
+                   }
+                   }
 
-
-
-    fun opengmail(view: View) {
-        val email:TextView=findViewById(R.id.editText1)
-        val subject:EditText=findViewById(R.id.editTextSubject)
-        val mes:EditText=findViewById(R.id.editTextMessage)
-        val sendmail:TextView=findViewById(R.id.textView)
-        ShareCompat.IntentBuilder.from(this).setText(mes.text.toString()).setSubject(subject.text.toString()).setEmailTo(arrayOf("sanafarooqui2002@gmail.com")).setChooserTitle("share to").setType("plain/text").startChooser()
-
-
-    }
-
-
-}
+               }
