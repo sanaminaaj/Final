af4ccfbee9d7a9d1140e2bde0573c54dad465904
sanaminaaj <86563339+sanaminaaj@users.noreply.github.com>
2021-07-25 19:59:51 +0530

    Update Main_Activity8.kt

--- Main_Activity8.kt
+++ Main_Activity8.kt
@@ -1,20 +1,22 @@
 package com.example.projectwork
 
+import android.content.Intent
+import android.database.Cursor
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import com.example.projectwork.DBHandler
 
 class MainActivity8 : AppCompatActivity() {
-    private lateinit var  courseNameEdt: EditText
+    private lateinit var courseNameEdt: EditText
     private lateinit var courseTracksEdt: EditText
-    private lateinit  var courseDurationEdt: EditText
-    private lateinit  var courseDescriptionEdt: EditText
-    private val addCourseBtn: Button =findViewById(R.id.idBtnAddCourse)
-    private var dbHandler: DBHandler? = null
+    private lateinit var courseDurationEdt: EditText
+    private lateinit var courseDescriptionEdt: EditText
+    private lateinit var addCourseBtn: Button
+    private lateinit var dbHandler: DataBaseHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main8)
@@ -24,31 +26,35 @@ class MainActivity8 : AppCompatActivity() {
         courseTracksEdt = findViewById(R.id.idEdtCourseTracks)
         courseDurationEdt = findViewById(R.id.idEdtCourseDuration)
         courseDescriptionEdt = findViewById(R.id.idEdtCourseDescription)
+        addCourseBtn = findViewById(R.id.idBtnAddCourse)
 
-        dbHandler = DBHandler(this)
-        val courseName = courseNameEdt.text.toString()
-        val courseTracks = courseTracksEdt.text.toString()
-        val courseDuration = courseDurationEdt.text.toString()
-        val courseDescription = courseDescriptionEdt.text.toString()
-
+        // creating a new dbhandler class
+        // and passing our context to it.
+        dbHandler = DataBaseHelper(this)
 
         // below line is to add on click listener for our add course button.
         addCourseBtn.setOnClickListener(View.OnClickListener { // below line is to get data from all edit text fields.
-            if (courseName.isEmpty() && courseTracks.isEmpty() && courseDuration.isEmpty() && courseDescription.isEmpty()) {
-                Toast.makeText(applicationContext, "Please enter all the data..", Toast.LENGTH_LONG).show()
-                return@OnClickListener
-            }
+            val courseName = courseNameEdt.text.toString()
+            val courseTracks = courseTracksEdt.text.toString()
+            val courseDuration = courseDurationEdt.text.toString()
+            val courseDescription = courseDescriptionEdt.text.toString()
 
+            // validating if the text fields are empty or not.
+            if (courseName.isNotEmpty() && courseTracks.isNotEmpty() && courseDuration.isNotEmpty() && courseDescription.isNotEmpty())
+            {
+                dbHandler.addNewCourse(courseName, courseDuration, courseDescription, courseTracks)
+                courseNameEdt.setText("")
+                courseDurationEdt.setText("")
+                courseTracksEdt.setText("")
+                courseDescriptionEdt.setText("")
+                Toast.makeText(this@MainActivity8, "Registration successful.", Toast.LENGTH_SHORT).show()
+                val i=Intent(this,MainActivity6::class.java)
+                startActivity(i)
+            }
             // on below line we are calling a method to add new
             // course to sqlite data and pass all our values to it.
-            dbHandler!!.addNewCourse(courseName, courseDuration, courseDescription, courseTracks)
-
-            // after adding the data we are displaying a toast message.
-            Toast.makeText(applicationContext,"Course has been added.", Toast.LENGTH_LONG).show()
-            courseNameEdt.setText("")
-            courseDurationEdt.setText("")
-            courseTracksEdt.setText("")
-            courseDescriptionEdt.setText("")
+            Toast.makeText(this@MainActivity8, "Please enter all the data..", Toast.LENGTH_SHORT).show()
+            return@OnClickListener
         })
-    }
-}
+
+    }}
