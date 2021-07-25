0262762a8dcbce8e60bb867a5839dbc36afc9583
sanaminaaj <86563339+sanaminaaj@users.noreply.github.com>
2021-07-25 19:55:50 +0530

    Update MainActivity5.kt

--- MainActivity5.kt
+++ MainActivity5.kt
@@ -11,11 +11,11 @@ class MainActivity5 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main9)
-        val login= findViewById(R.id.login) as Button
+        val login= findViewById<Button>(R.id.login)
         login.setOnClickListener{
-            val name=findViewById(R.id.text1) as EditText
-            val password=findViewById(R.id.text2) as EditText
-            if(name.text.toString().equals("student")&&password.text.toString().equals("student"))
+            val name= findViewById<EditText>(R.id.text1)
+            val password= findViewById<EditText>(R.id.text2)
+            if(name.text.toString() == "student" && password.text.toString() == "student")
             {
                 Toast.makeText(applicationContext, "This is a valid ", Toast.LENGTH_LONG).show()
                 val intent= Intent(this,MainActivity8::class.java)
